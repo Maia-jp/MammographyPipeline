f7aa6e31f8261a9f73190e6c0f0cd0c6a8c69151
JP Maia <maiajp2305@gmail.com>
2023-09-13 12:59:07 -0300

    Trying to fix model to onx

--- collab.sh
+++ collab.sh
@@ -1,7 +1,7 @@
-pip install -r /content/drive/MyDrive/Faculdade/TCC/sample_code/mammography_segmentation/requirements.txt
+pip install -r /MammographyPipeline/requirements.txt
 pip install onnx
 pip install git+https://github.com/microsoft/onnxconverter-common
 pip install git+https://github.com/onnx/onnxmltools
+pip install git+https://github.com/onnx/tensorflow-onnx
 pip install -U -q segmentation-models
 pip install python-dotenv
-pip install tf2onnx
