b4a4851ca56e9973292688236deaab87c1a53a06
JP Maia <maiajp2305@gmail.com>
2023-09-13 13:03:43 -0300

    fix sh

--- collab.sh
+++ collab.sh
@@ -1,4 +1,4 @@
-pip install -r /MammographyPipeline/requirements.txt
+pip install -r ./MammographyPipeline/requirements.txt
 pip install onnx
 pip install git+https://github.com/microsoft/onnxconverter-common
 pip install git+https://github.com/onnx/onnxmltools
