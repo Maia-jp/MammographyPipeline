f0e9b0e908317d8b40af4e21d0e049b79ecc0b46
JP Maia <maiajp2305@gmail.com>
2024-01-31 15:43:00 -0300

    Downgrading version of progressbar

--- collab.sh
+++ collab.sh
@@ -6,3 +6,4 @@ pip install git+https://github.com/onnx/onnxmltools
 pip install git+https://github.com/onnx/tensorflow-onnx
 pip install -U -q segmentation-models
 pip install python-dotenv
+pip install progressbar2
